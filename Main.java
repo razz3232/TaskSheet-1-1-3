b37690a1bd82cb1e9e2d0410890c218d9f16a2a3
razz3232 <74662119+razz3232@users.noreply.github.com>
2023-12-29 06:20:00 +0000

    output done

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+public class Main {
+    public static void main(String[]args){
+        int checkNumber = 10;
+        String message;
+        
+        for (int i = 1; i <= checkNumber; i++){
+            message = (i%2==0) ? i+ " is even number" : i+" is odd number";
+            System.out.println(message);
+        }
+        
 
+        
+
+    }
+
+}
